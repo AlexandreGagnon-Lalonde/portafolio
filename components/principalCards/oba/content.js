f0c30cd201ee83e82c5a694bcdabe1ac9c09008d
AlexandreGagnon-Lalonde <alexandre.gl@hotmail.ca>
2020-11-27 13:11:31 -0500

    project content section basic style

--- components/principalCards/oba/content.js
+++ components/principalCards/oba/content.js
@@ -1,14 +1,18 @@
 import styled from "styled-components";
 
+import ContentText from '../Content/contentText';
+import ContentImage from '../Content/contentImage';
+
 export default function Content() {
   return <InfoContainer>
-      lessons
+      <ContentText />
+      <ContentImage src={'/assets/3projfill.jpg'}/>
     </InfoContainer>
 }
 
 const InfoContainer = styled.div`
+display: grid;
+grid-template-columns: 1fr 1fr;
   width: 100%;
   height: 100vh;
-  background-color: gray;
-  color: white;
 `
