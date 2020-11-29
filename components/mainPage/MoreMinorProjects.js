831c269ceced32d0c0b9e228e6add55780d980f2
AlexandreGagnon-Lalonde <alexandre.gl@hotmail.ca>
2020-11-29 17:15:42 -0500

    archive link style

--- components/mainPage/MoreMinorProjects.js
+++ components/mainPage/MoreMinorProjects.js
@@ -2,11 +2,13 @@ import React from "react";
 import styled from "styled-components";
 import Link from "next/link";
 
+import { COLORS } from "../../public/constant";
+
 const MoreMinorProjects = () => {
   return (
     <Container>
       <MoreProjectsLink href={"/archive"} passHref>
-        <ArchiveLink>Archive of minor projects</ArchiveLink>
+        <ArchiveLink>Click down here to see the archive</ArchiveLink>
       </MoreProjectsLink>
     </Container>
   );
@@ -15,6 +17,13 @@ const MoreMinorProjects = () => {
 const Container = styled.div`
   grid-row: 1 / 3;
   grid-column: 2;
+  background: ${COLORS.blue};
+  background: radial-gradient(ellipse at right, ${COLORS.blue} 0%, ${COLORS.white} 50%);
+  transition: all 0.2s;
+
+  &:hover {
+    background: radial-gradient(ellipse at right, ${COLORS.blue} 30%, ${COLORS.white} 65%);
+  }
 `
 const MoreProjectsLink = styled(Link)`
   height: 100%;
@@ -22,6 +31,8 @@ const MoreProjectsLink = styled(Link)`
 const ArchiveLink = styled.a`
 display: block;
   height: 100%;
+  text-decoration: none;
+  color: ${COLORS.black};
 `
 
 export default MoreMinorProjects;
