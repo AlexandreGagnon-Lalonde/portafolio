eee36d4e599ead3dc1feb7d592ae675c2b1d3c14
AlexandreGagnon-Lalonde <alexandre.gl@hotmail.ca>
2020-12-01 20:25:05 -0500

    contact me section roudned border

--- components/contact/ContactMe.js
+++ components/contact/ContactMe.js
@@ -18,7 +18,9 @@ export default function ContactMe () {
 };
 
 const ContactContainer = styled.div`
-margin-top: 40px;
+margin-top: 80px;
+border-top-left-radius: 40px;
+border-top-right-radius: 40px;
 background: ${COLORS.blue};
 background: linear-gradient(0deg, ${COLORS.blue} 90%, rgba(251,246,240,1) 100%);
 `
