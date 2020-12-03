4ce8fa63b9b18061de680419109a445c7ed7ddfe
AlexandreGagnon-Lalonde <alexandre.gl@hotmail.ca>
2020-12-03 11:33:55 -0500

    add email link

--- components/contact/PlatformLink.js
+++ components/contact/PlatformLink.js
@@ -3,6 +3,7 @@ import Link from "next/link";
 
 import { VscGithubInverted, VscGithub } from "react-icons/vsc";
 import { SiLinkedin } from "react-icons/si";
+import { HiOutlineMail } from 'react-icons/hi'
 
 import { COLORS } from "../../public/constant";
 
@@ -11,26 +12,28 @@ export default function PlatformLinks () {
     <LinkyContainer>
       <Linky href={'https://github.com/AlexandreGagnon-Lalonde'}><VscGithubInverted style={{ fontSize: '1.5em'}} /></Linky>
       <Linky href={'https://www.linkedin.com/in/alexandregagnonlalonde/'}><SiLinkedin style={{ fontSize: '1.5em'}} /></Linky>
+      <Linky href={'mailto:alexandregagnonlalonde@gmail.com'}><HiOutlineMail style={{ fontSize: '1.5em'}} /></Linky>
     </LinkyContainer>
   );
 };
 
 const LinkyContainer = styled.div`
-  width: 65px;
-  padding: 10px;
+  width: 100%;
+  padding: 40px 0 20px 0;
   display: flex;
-  flex-direction: column;
   align-items: center;
   justify-content: space-around;
-  width: 15%;
 `
 const Linky = styled.a`
   color: ${COLORS.white};
   background-color: ${COLORS.orange};
-  padding: 20px;
+  padding: 15px;
   border-radius: 10px;
   opacity: 0.8;
   transition: all 0.2s;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
   &:hover {
     opacity: 1;
