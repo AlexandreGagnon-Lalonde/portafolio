5c99f9df09e10a04ddb9ccd02ff756d4ac9ba503
AlexandreGagnon-Lalonde <alexandre.gl@hotmail.ca>
2020-12-07 20:33:08 -0500

    purpose text

--- components/principalCards/Lesson/lessonText.js
+++ components/principalCards/Lesson/lessonText.js
@@ -2,8 +2,8 @@ import styled from "styled-components";
 
 export default function LessonText() {
   return <InfoContainer>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh.
-    </InfoContainer>
+      better planification for server, packages,
+      </InfoContainer>
 }
 
 const InfoContainer = styled.div`
--- components/principalCards/Purpose/purposeText.js
+++ components/principalCards/Purpose/purposeText.js
@@ -1,8 +1,11 @@
 import styled from "styled-components";
 
+import { COLORS } from '../../../public/constant';
+
 export default function PurposeText() {
   return <InfoContainer>
-      <PurposeParagraph></PurposeParagraph>
+      <PurposeParagraph>The project is also adapted for a seemless mobile experience. The UI is pretty simple, all the boxes of content are in order of importance. Dark mode is rather popular these days. I decided to go with only the dark theme to save the users eyes since adding that functionality would've made me late for the deadline.</PurposeParagraph>
+      <PurposeParagraph>MongoDB is the main database used to contain the data useful for the users and the classes. It would be interesting to see if SQL would be a better use rather than collecting all the users data for a specific information. The interactions between the front-end, the back-end and the database were quite straightforward, but the main issue is to structure all those interactions together to make a smooth relation between the stacks.</PurposeParagraph>
     </InfoContainer>
 }
 
@@ -16,5 +19,4 @@ text-indent: 2em;
 text-align: justify;
 line-height: 2;
 font-weight: 600;
-
 `
