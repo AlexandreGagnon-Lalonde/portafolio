c99215ee908cc6a7e72dbdf49c17489792313dec
AlexandreGagnon-Lalonde <alexandre.gl@hotmail.ca>
2020-12-19 14:36:47 -0500

    js to jsx navigation

--- components/navigation/projectsLink.jsx
+++ components/navigation/projectsLink.jsx
@@ -1,7 +1,5 @@
 import styled from "styled-components";
 import Link from "next/link";
-import { useRouter } from "next/router";
-
 import { COLORS } from "../../public/constant";
 
 export default function ProjectsLink() {
