88619bac391a0fecc0404b7e44975c0eaaa7a9b6
AlexandreGagnon-Lalonde <alexandre.gl@hotmail.ca>
2020-12-24 14:43:05 -0500

    global light and dark mode

---
+++ styles/global.js
@@ -0,0 +1,92 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  html, body {
+    max-width: 100vw;
+    overflow-x: hidden;
+  }
+  body {
+    transition: all 0.3s;
+    line-height: 1.25;
+    color: ${(props) => props.theme.text};
+    background: ${(props) => props.theme.body};
+  }
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+  }
+  a {
+    text-decoration: none;
+    color: inherit;
+    cursor: pointer;
+  }
+  button {
+    background-color: transparent;
+    color: inherit;
+    border-width: 0;
+    padding: 0;
+    cursor: pointer;
+  }
+  figure {
+    margin: 0;
+  }
+  input::-moz-focus-inner {
+    border: 0;
+    padding: 0;
+    margin: 0;
+  }
+
+  ul,
+  ol,
+  dd {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    margin: 0;
+  }
+  p {
+    margin: 0;
+  }
+  cite {
+    font-style: normal;
+  }
+  fieldset {
+    border-width: 0;
+    padding: 0;
+    margin: 0;
+  }
+  ::-webkit-scrollbar {
+    width: 10px;
+  }
+  ::-webkit-scrollbar-track {
+    background: #aee6e6;
+  }
+  ::-webkit-scrollbar-thumb {
+    background: #ffda77;
+    border-radius: 5px;
+  }
+  ::-webkit-scrollbar-thumb:hover {
+    background: #ffc832;
+  }
+  .carousel-control-prev-icon {
+    transform: scale(1.5);
+    background-image: url("data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='%23${props => props.theme.carouselArrow.substring(1)}' viewBox='0 0 8 8'%3E%3Cpath d='M5.25 0l-4 4 4 4 1.5-1.5-2.5-2.5 2.5-2.5-1.5-1.5z'/%3E%3C/svg%3E");
+  }
+  .carousel-control-next-icon {
+    transform: scale(1.5);
+    background-image: url("data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='%23${props => props.theme.carouselArrow.substring(1)}' viewBox='0 0 8 8'%3E%3Cpath d='M2.75 0l-1.5 1.5 2.5 2.5-2.5 2.5 1.5 1.5 4-4-4-4z'/%3E%3C/svg%3E");
+  }
+  .carousel-indicators li {
+    background-color: black;
+    border-radius: 1px;
+  }
+
+`;
