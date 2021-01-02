3aafce6ef4c92a603c57ccecef7dedcfa13a7e04
AlexandreGagnon-Lalonde <alexandregagnonlalonde@gmail.com>
2021-01-02 09:31:21 -0500

    project page title

--- pages/projects/ECommerce/index.jsx
+++ pages/projects/ECommerce/index.jsx
@@ -11,6 +11,7 @@ export default function ECommerce() {
       </Head>
       <Navigation />
       <ScrollToTopButton />
+      <AppTitle>Wearables E-Commerce</AppTitle>
     </>
   );
 }
@@ -19,5 +20,26 @@ const FirstPartOfProject = styled.div`
   display: flex;
   @media (max-width: 768px) {
     flex-direction: column;
+  }
+`;
+const AppTitle = styled.h3`
+  width: 100%;
+  text-shadow: 0px 0px 2px #101010;
+  font-family: Georgia, serif;
+  font-size: 1.5em;
+  letter-spacing: 3px;
+  word-spacing: 2px;
+  color: #101010;
+  font-weight: 700;
+  text-decoration: none;
+  font-style: normal;
+  font-variant: small-caps;
+  text-transform: none;
+  margin: 60px 0 40px 60px;
+  color: ${props => props.theme.projectPageTitle};
+
+  @media (max-width: 768px) {
+    margin: 60px auto 40px auto;
+    text-align: center;
   }
 `;
